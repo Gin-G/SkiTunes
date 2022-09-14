d77dcfe13696c53ccda0e332258322ba9aa03ce8
Gin-G <NickCo7@gmail.com>
2022-09-13 19:54:17 -0600

    good update with new search

--- skitunes/skitunes/main/views.py
+++ skitunes/skitunes/main/views.py
@@ -47,14 +47,62 @@ def skibase_lite():
 def findmovie():
     song_name = request.args.get('song_name')
     song_artist = request.args.get('song_artist')
-    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_name.like(song_name),ski_movie_song_info.song_artist.like(song_artist))
+    print(song_name,song_artist)
+    if len(song_artist) == 0:
+        filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_name.like(song_name))
+    elif len(song_name) == 0:
+        filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_artist.like(song_artist))
+    else:
+        filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_name.like(song_name),ski_movie_song_info.song_artist.like(song_artist))
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filtermovie')
+def filter_movie():
+    movie_name = request.args.get("movie_name")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.movie_name == movie_name)
     return render_template('skibase.html', tracks = filter_info)
 
 @app.route('/skitunes/filterskier')
 def filter():
-    query = request.args.get('query')
-    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.skier_name == query)
-    return render_template('skitunes.html', tracks = filter_info)
+    skier_name = request.args.get('skier_name')
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.skier_name == skier_name)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filtersong')
+def filter_song():
+    song_name = request.args.get("song_name")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_name == song_name)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filterartist')
+def filter_artist():
+    song_artist = request.args.get("song_artist")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_artist == song_artist)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filteralbum')
+def filter_album():
+    song_album = request.args.get("song_album")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.song_album == song_album)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filtermovieco')
+def filter_movieco():
+    movie_co = request.args.get("movie_co")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.movie_co == movie_co)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filterlocation')
+def filter_location():
+    location = request.args.get("location")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.location == location)
+    return render_template('skibase.html', tracks = filter_info)
+
+@app.route('/skitunes/filtertype')
+def filter_type():
+    ski_type = request.args.get("ski_type")
+    filter_info = ski_movie_song_info.query.filter(ski_movie_song_info.ski_type == ski_type)
+    return render_template('skibase.html', tracks = filter_info)
 
 @app.route('/skitunes/likewhat')
 def like_what():
@@ -66,7 +114,7 @@ def new_entry():
         if not request.form['song_name']:
             flash('Please enter all the fields', 'error')
         else:
-            movie_song_info = ski_movie_song_info(song_name = request.form['song_name'], song_artist = request.form['song_artist'], skier_name = request.form['skier_name'], movie_name = request.form['movie_name'], ski_type = request.form['ski_type'], video_link = request.form['video_link'])
+            movie_song_info = ski_movie_song_info(song_name = request.form['song_name'], song_artist = request.form['song_artist'], song_album = request.form['song_album'], song_num=request.form['song_num'], spotify_id=request.form['spotify_link'], skier_name = request.form['skier_name'], movie_name = request.form['movie_name'], movie_year=request.form['movie_year'], movie_co=request.form['movie_co'], ski_type = request.form['ski_type'], location=request.form['location'], video_link = request.form['video_link'])           
             db.session.add(movie_song_info)
             db.session.commit()
 
@@ -90,7 +138,7 @@ def bulk_import():
         ski_type = track["Skiing type"]
         skier_name = track["Skier Name(s)"]
         location = track["Location"]
-        video_link  = 'None'
+        video_link  = 'Unknown'
         movie_song_info = ski_movie_song_info(song_name = song_name, song_artist = song_artist, song_album = song_album, song_num=song_num, spotify_id=spotify_link, skier_name = skier_name, movie_name = movie_name, movie_year=movie_year, movie_co=movie_co, ski_type = ski_type, location=location, video_link = video_link)
         db.session.add(movie_song_info)
         db.session.commit()
