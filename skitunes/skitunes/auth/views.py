e92624ed4f95fd21b8beaf17153184ad8c45633c
cnick <cnick@netapp.com>
2022-11-15 13:12:47 -0700

    fix?

--- skitunes/skitunes/auth/views.py
+++ skitunes/skitunes/auth/views.py
@@ -64,7 +64,7 @@ def callback():
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
         authorization_response=request.url,
-        redirect_url="https://www.skimoviesongs.com/login",
+        redirect_url="https://www.skimoviesongs.com/login/callback",
         code=code
     )
     token_response = requests.post(
