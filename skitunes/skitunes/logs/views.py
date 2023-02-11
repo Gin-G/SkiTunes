72b5376bcd9b11886f239123cab83132990a73cb
Nick Cote <NickCo7@gmail.com>
2023-02-11 14:05:55 -0700

    change log path

--- skitunes/skitunes/logs/views.py
+++ skitunes/skitunes/logs/views.py
@@ -6,16 +6,18 @@ from skitunes import app
 # Flask view to list all reports in the directory
 @app.route("/Reports")
 def view_reports():
+    prefix = os.getcwd()
     # Set the files as everything in the directory
-    files = os.listdir('./log/')
+    files = os.listdir(prefix + '/log/')
     # Return the reports.html file with all the files and set our Filter to None
     return render_template('reports.html', files=files, filter=None)
 
 # Flask view to view the individual report in the UI
 @app.route("/Reports/<filename>")
 def view_report(filename):
+    prefix = os.getcwd()
     # Open the report file
-    with open('./log/' + filename, "r") as report:
+    with open(prefix + '/log/' + filename, "r") as report:
         # Read the report and save it as a string
         string = report.read()
     # Return the report.html with the file being the strign read of the file
